57d3f2b681466c6661b7d0e8399092c7b850e970
FawziaAlushrafii <Fawzia.Hassan.ruh.k@tuwaiq.edu.sa>
2021-10-20 12:54:28 +0300

    file renane

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,51 +1,16 @@
-fun main(args: Array<String>) {
-
-    var healthPoints = 60
-    var isBlessed = true
-    var name = "Arua"
-    var isImmortal = false
-
-    if ( isBlessed && healthPoints > 60 ||  isImmortal) {}
-    val auraVisible = isBlessed && healthPoints > 60 ||  isImmortal
-    val auraColor = if (auraVisible) "GREEN" else "NONE"
-
-    val healthStatus: String = when (healthPoints) {
-        100 -> "excellent "
-        in 90..99 -> "has a few scratches."
-        in 75..89 -> if (isBlessed) {
-            "has some minor wounds but is healing quite quickly!"
-        } else {
-            "has some minor wounds."
-        }
-        in 15..74 -> "looks pretty hurt."
-        else -> "is in awful condition"
-    }
-
-    println("(Aura: $auraColor) "
-            + "(Blessed: ${if (isBlessed) "YES" else "NO"})")
-    println("$name $healthStatus")
-
-
-    val karma = (Math.pow(Math.random(), (110 - healthPoints) / 100.0) * 20 ).toInt()
-
-    var KarmaValue = 3
-    when(KarmaValue) {
-        in 0..5 -> {
-            println("red ")
-        }
-        in 6..10 -> {
-            println("orange")
-        }
-        in 11..15  -> {
-            println("Purple")
-        }
-        in 16..20 -> {
-            println("Green ")
+fun main(args:Array<String>) {
+    var isAura ="green"
+    val PlyerHealthMadigal=89
+    val plyerkarma = (Math.pow(Math.random(), (100 - PlyerHealthMadigal) / 100.0) * 20 ).toInt()
+    println("the player color is :$isAura")
+    when(PlyerHealthMadigal) {
+        in 0..5 -> isAura = "red"
+        in 6..10 -> isAura = "orange"
+        in 11..15  -> isAura ="purple"
+        in 16..20 -> isAura = "green"
         }
 
-        else -> {
-            print("visible")
+            println("Aur Now is :$isAura")
+            println("visible")
         }
-    }
 
-}
